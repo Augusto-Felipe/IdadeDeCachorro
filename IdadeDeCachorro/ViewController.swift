4fee02a34909ffa34ee9206012f3aa3014b6c274
Felipe Augusto <augusto_felipe@icloud.com>
2022-08-30 19:04:23 -0300

    Botão Calcular Idade

--- IdadeDeCachorro/ViewController.swift
+++ IdadeDeCachorro/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var legendaResultado: UILabel!
+    
+    //Executa sempre que a tela é carregada
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    //Botão Calcular Idade
+    @IBAction func calcularIdade(_ sender: Any) {
+        
+        legendaResultado.text = "A idade do cachorro em anos humanos é: 14"
+        
+    }
 }
 
